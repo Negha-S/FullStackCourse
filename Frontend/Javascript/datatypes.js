932c215d76f5f64b686708ffccc987a994e71984
Negha S <neghas26@gmail.com>
2026-01-09 16:06:57 +0530

    Add files via upload

---
+++ Frontend/Javascript/datatypes.js
@@ -0,0 +1,10 @@
+// let x;
+// x=25;
+// console.log(x);
+
+const y=11;
+console.log(y);
+
+// z=10;
+// var z;
+// console.log(z);
---
+++ Frontend/Javascript/print1.js
@@ -0,0 +1,7 @@
+console.log("Good Day");
+var a=2;
+var b=4;
+console.log("Add:",a+b);
+console.log("Sub:",a-b);
+console.log("Mul:",a*b);
+console.log("Div:",a/b);
